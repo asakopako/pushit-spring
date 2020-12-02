1707f739b096a546127409a59101ce28652d5e9f
asakopako <asakopako86@gmail.com>
2020-12-02 13:53:35 +0100

    Initial import

---
+++ src/main/java/com/rigby/pushit/PushItApplication.java
@@ -0,0 +1,13 @@
+package com.rigby.pushit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rigby/pushit/PushItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigby.pushit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
